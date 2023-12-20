c3896141b965df5510fda2750f6719ee2ea8f24f
Ben Monro <benjamin@benmonro.net>
2023-12-20 17:11:55 +1100

    Display of all player records onto form (name, score, time)

--- Battleship/HighScoresForm.Designer.vb
+++ Battleship/HighScoresForm.Designer.vb
@@ -24,8 +24,55 @@ Partial Class HighScoresForm
     Private Sub InitializeComponent()
         WaterBoarder = New PictureBox()
         backtomainbtn = New Button()
-        ListBox1 = New ListBox()
+        p1namelbl = New Label()
+        p3namelbl = New Label()
+        p2namelbl = New Label()
+        ranklbl1 = New Label()
+        ranklbl2 = New Label()
+        ranklbl3 = New Label()
+        ranklbl4 = New Label()
+        ranklbl5 = New Label()
+        ranklbl6 = New Label()
+        ranklbl7 = New Label()
+        ranklbl8 = New Label()
+        ranklbl9 = New Label()
+        ranklbl10 = New Label()
+        p4namelbl = New Label()
+        p5namelbl = New Label()
+        p6namelbl = New Label()
+        p7namelbl = New Label()
+        p8namelbl = New Label()
+        p9namelbl = New Label()
+        p10namelbl = New Label()
+        rankingpanel = New Panel()
+        namepanel = New Panel()
+        p1scorelbl = New Label()
+        p2scorelbl = New Label()
+        p3scorelbl = New Label()
+        p4scorelbl = New Label()
+        p5scorelbl = New Label()
+        p6scorelbl = New Label()
+        p7scorelbl = New Label()
+        p8scorelbl = New Label()
+        p9scorelbl = New Label()
+        p10scorelbl = New Label()
+        scorepanel = New Panel()
+        p1timelbl = New Label()
+        p2timelbl = New Label()
+        p3timelbl = New Label()
+        p6timelbl = New Label()
+        p5timelbl = New Label()
+        p4timelbl = New Label()
+        p9timelbl = New Label()
+        p8timelbl = New Label()
+        p7timelbl = New Label()
+        p10timelbl = New Label()
+        timepanel = New Panel()
         CType(WaterBoarder, ComponentModel.ISupportInitialize).BeginInit()
+        rankingpanel.SuspendLayout()
+        namepanel.SuspendLayout()
+        scorepanel.SuspendLayout()
+        timepanel.SuspendLayout()
         SuspendLayout()
         ' 
         ' WaterBoarder
@@ -56,14 +103,433 @@ Partial Class HighScoresForm
         backtomainbtn.Text = "EXIT"
         backtomainbtn.UseVisualStyleBackColor = False
         ' 
-        ' ListBox1
+        ' p1namelbl
         ' 
-        ListBox1.FormattingEnabled = True
-        ListBox1.ItemHeight = 15
-        ListBox1.Location = New Point(396, 158)
-        ListBox1.Name = "ListBox1"
-        ListBox1.Size = New Size(221, 184)
-        ListBox1.TabIndex = 8
+        p1namelbl.AutoSize = True
+        p1namelbl.Location = New Point(20, 16)
+        p1namelbl.Name = "p1namelbl"
+        p1namelbl.Size = New Size(45, 15)
+        p1namelbl.TabIndex = 9
+        p1namelbl.Text = "player1"
+        ' 
+        ' p3namelbl
+        ' 
+        p3namelbl.AutoSize = True
+        p3namelbl.Location = New Point(20, 56)
+        p3namelbl.Name = "p3namelbl"
+        p3namelbl.Size = New Size(45, 15)
+        p3namelbl.TabIndex = 10
+        p3namelbl.Text = "player3"
+        ' 
+        ' p2namelbl
+        ' 
+        p2namelbl.AutoSize = True
+        p2namelbl.Location = New Point(20, 36)
+        p2namelbl.Name = "p2namelbl"
+        p2namelbl.Size = New Size(45, 15)
+        p2namelbl.TabIndex = 11
+        p2namelbl.Text = "player2"
+        ' 
+        ' ranklbl1
+        ' 
+        ranklbl1.AutoSize = True
+        ranklbl1.Location = New Point(27, 15)
+        ranklbl1.Name = "ranklbl1"
+        ranklbl1.Size = New Size(13, 15)
+        ranklbl1.TabIndex = 12
+        ranklbl1.Text = "1"
+        ' 
+        ' ranklbl2
+        ' 
+        ranklbl2.AutoSize = True
+        ranklbl2.Location = New Point(27, 35)
+        ranklbl2.Name = "ranklbl2"
+        ranklbl2.Size = New Size(13, 15)
+        ranklbl2.TabIndex = 13
+        ranklbl2.Text = "2"
+        ' 
+        ' ranklbl3
+        ' 
+        ranklbl3.AutoSize = True
+        ranklbl3.Location = New Point(27, 55)
+        ranklbl3.Name = "ranklbl3"
+        ranklbl3.Size = New Size(13, 15)
+        ranklbl3.TabIndex = 14
+        ranklbl3.Text = "3"
+        ' 
+        ' ranklbl4
+        ' 
+        ranklbl4.AutoSize = True
+        ranklbl4.Location = New Point(27, 75)
+        ranklbl4.Name = "ranklbl4"
+        ranklbl4.Size = New Size(13, 15)
+        ranklbl4.TabIndex = 15
+        ranklbl4.Text = "4"
+        ' 
+        ' ranklbl5
+        ' 
+        ranklbl5.AutoSize = True
+        ranklbl5.Location = New Point(27, 95)
+        ranklbl5.Name = "ranklbl5"
+        ranklbl5.Size = New Size(13, 15)
+        ranklbl5.TabIndex = 16
+        ranklbl5.Text = "5"
+        ' 
+        ' ranklbl6
+        ' 
+        ranklbl6.AutoSize = True
+        ranklbl6.Location = New Point(27, 115)
+        ranklbl6.Name = "ranklbl6"
+        ranklbl6.Size = New Size(13, 15)
+        ranklbl6.TabIndex = 17
+        ranklbl6.Text = "6"
+        ' 
+        ' ranklbl7
+        ' 
+        ranklbl7.AutoSize = True
+        ranklbl7.Location = New Point(27, 135)
+        ranklbl7.Name = "ranklbl7"
+        ranklbl7.Size = New Size(13, 15)
+        ranklbl7.TabIndex = 18
+        ranklbl7.Text = "7"
+        ' 
+        ' ranklbl8
+        ' 
+        ranklbl8.AutoSize = True
+        ranklbl8.Location = New Point(27, 155)
+        ranklbl8.Name = "ranklbl8"
+        ranklbl8.Size = New Size(13, 15)
+        ranklbl8.TabIndex = 19
+        ranklbl8.Text = "8"
+        ' 
+        ' ranklbl9
+        ' 
+        ranklbl9.AutoSize = True
+        ranklbl9.Location = New Point(27, 175)
+        ranklbl9.Name = "ranklbl9"
+        ranklbl9.Size = New Size(13, 15)
+        ranklbl9.TabIndex = 20
+        ranklbl9.Text = "9"
+        ' 
+        ' ranklbl10
+        ' 
+        ranklbl10.AutoSize = True
+        ranklbl10.Location = New Point(27, 195)
+        ranklbl10.Name = "ranklbl10"
+        ranklbl10.Size = New Size(19, 15)
+        ranklbl10.TabIndex = 21
+        ranklbl10.Text = "10"
+        ' 
+        ' p4namelbl
+        ' 
+        p4namelbl.AutoSize = True
+        p4namelbl.Location = New Point(20, 76)
+        p4namelbl.Name = "p4namelbl"
+        p4namelbl.Size = New Size(45, 15)
+        p4namelbl.TabIndex = 22
+        p4namelbl.Text = "player4"
+        ' 
+        ' p5namelbl
+        ' 
+        p5namelbl.AutoSize = True
+        p5namelbl.Location = New Point(20, 96)
+        p5namelbl.Name = "p5namelbl"
+        p5namelbl.Size = New Size(45, 15)
+        p5namelbl.TabIndex = 23
+        p5namelbl.Text = "player5"
+        ' 
+        ' p6namelbl
+        ' 
+        p6namelbl.AutoSize = True
+        p6namelbl.Location = New Point(20, 116)
+        p6namelbl.Name = "p6namelbl"
+        p6namelbl.Size = New Size(45, 15)
+        p6namelbl.TabIndex = 24
+        p6namelbl.Text = "player6"
+        ' 
+        ' p7namelbl
+        ' 
+        p7namelbl.AutoSize = True
+        p7namelbl.Location = New Point(20, 136)
+        p7namelbl.Name = "p7namelbl"
+        p7namelbl.Size = New Size(45, 15)
+        p7namelbl.TabIndex = 25
+        p7namelbl.Text = "player7"
+        ' 
+        ' p8namelbl
+        ' 
+        p8namelbl.AutoSize = True
+        p8namelbl.Location = New Point(20, 156)
+        p8namelbl.Name = "p8namelbl"
+        p8namelbl.Size = New Size(45, 15)
+        p8namelbl.TabIndex = 26
+        p8namelbl.Text = "player8"
+        ' 
+        ' p9namelbl
+        ' 
+        p9namelbl.AutoSize = True
+        p9namelbl.Location = New Point(20, 176)
+        p9namelbl.Name = "p9namelbl"
+        p9namelbl.Size = New Size(45, 15)
+        p9namelbl.TabIndex = 27
+        p9namelbl.Text = "player9"
+        ' 
+        ' p10namelbl
+        ' 
+        p10namelbl.AutoSize = True
+        p10namelbl.Location = New Point(20, 196)
+        p10namelbl.Name = "p10namelbl"
+        p10namelbl.Size = New Size(51, 15)
+        p10namelbl.TabIndex = 28
+        p10namelbl.Text = "player10"
+        ' 
+        ' rankingpanel
+        ' 
+        rankingpanel.Controls.Add(ranklbl10)
+        rankingpanel.Controls.Add(ranklbl9)
+        rankingpanel.Controls.Add(ranklbl8)
+        rankingpanel.Controls.Add(ranklbl7)
+        rankingpanel.Controls.Add(ranklbl6)
+        rankingpanel.Controls.Add(ranklbl5)
+        rankingpanel.Controls.Add(ranklbl4)
+        rankingpanel.Controls.Add(ranklbl3)
+        rankingpanel.Controls.Add(ranklbl2)
+        rankingpanel.Controls.Add(ranklbl1)
+        rankingpanel.Location = New Point(80, 345)
+        rankingpanel.Name = "rankingpanel"
+        rankingpanel.Size = New Size(73, 236)
+        rankingpanel.TabIndex = 29
+        ' 
+        ' namepanel
+        ' 
+        namepanel.Controls.Add(p10namelbl)
+        namepanel.Controls.Add(p9namelbl)
+        namepanel.Controls.Add(p8namelbl)
+        namepanel.Controls.Add(p7namelbl)
+        namepanel.Controls.Add(p6namelbl)
+        namepanel.Controls.Add(p5namelbl)
+        namepanel.Controls.Add(p4namelbl)
+        namepanel.Controls.Add(p2namelbl)
+        namepanel.Controls.Add(p3namelbl)
+        namepanel.Controls.Add(p1namelbl)
+        namepanel.Location = New Point(172, 344)
+        namepanel.Name = "namepanel"
+        namepanel.Size = New Size(85, 238)
+        namepanel.TabIndex = 30
+        ' 
+        ' p1scorelbl
+        ' 
+        p1scorelbl.AutoSize = True
+        p1scorelbl.Location = New Point(11, 15)
+        p1scorelbl.Name = "p1scorelbl"
+        p1scorelbl.Size = New Size(13, 15)
+        p1scorelbl.TabIndex = 29
+        p1scorelbl.Text = "0"
+        ' 
+        ' p2scorelbl
+        ' 
+        p2scorelbl.AutoSize = True
+        p2scorelbl.Location = New Point(11, 35)
+        p2scorelbl.Name = "p2scorelbl"
+        p2scorelbl.Size = New Size(13, 15)
+        p2scorelbl.TabIndex = 31
+        p2scorelbl.Text = "0"
+        ' 
+        ' p3scorelbl
+        ' 
+        p3scorelbl.AutoSize = True
+        p3scorelbl.Location = New Point(11, 55)
+        p3scorelbl.Name = "p3scorelbl"
+        p3scorelbl.Size = New Size(13, 15)
+        p3scorelbl.TabIndex = 32
+        p3scorelbl.Text = "0"
+        ' 
+        ' p4scorelbl
+        ' 
+        p4scorelbl.AutoSize = True
+        p4scorelbl.Location = New Point(11, 75)
+        p4scorelbl.Name = "p4scorelbl"
+        p4scorelbl.Size = New Size(13, 15)
+        p4scorelbl.TabIndex = 33
+        p4scorelbl.Text = "0"
+        ' 
+        ' p5scorelbl
+        ' 
+        p5scorelbl.AutoSize = True
+        p5scorelbl.Location = New Point(11, 95)
+        p5scorelbl.Name = "p5scorelbl"
+        p5scorelbl.Size = New Size(13, 15)
+        p5scorelbl.TabIndex = 34
+        p5scorelbl.Text = "0"
+        ' 
+        ' p6scorelbl
+        ' 
+        p6scorelbl.AutoSize = True
+        p6scorelbl.Location = New Point(11, 115)
+        p6scorelbl.Name = "p6scorelbl"
+        p6scorelbl.Size = New Size(13, 15)
+        p6scorelbl.TabIndex = 35
+        p6scorelbl.Text = "0"
+        ' 
+        ' p7scorelbl
+        ' 
+        p7scorelbl.AutoSize = True
+        p7scorelbl.Location = New Point(11, 135)
+        p7scorelbl.Name = "p7scorelbl"
+        p7scorelbl.Size = New Size(13, 15)
+        p7scorelbl.TabIndex = 36
+        p7scorelbl.Text = "0"
+        ' 
+        ' p8scorelbl
+        ' 
+        p8scorelbl.AutoSize = True
+        p8scorelbl.Location = New Point(11, 155)
+        p8scorelbl.Name = "p8scorelbl"
+        p8scorelbl.Size = New Size(13, 15)
+        p8scorelbl.TabIndex = 37
+        p8scorelbl.Text = "0"
+        ' 
+        ' p9scorelbl
+        ' 
+        p9scorelbl.AutoSize = True
+        p9scorelbl.Location = New Point(11, 175)
+        p9scorelbl.Name = "p9scorelbl"
+        p9scorelbl.Size = New Size(13, 15)
+        p9scorelbl.TabIndex = 38
+        p9scorelbl.Text = "0"
+        ' 
+        ' p10scorelbl
+        ' 
+        p10scorelbl.AutoSize = True
+        p10scorelbl.Location = New Point(11, 195)
+        p10scorelbl.Name = "p10scorelbl"
+        p10scorelbl.Size = New Size(13, 15)
+        p10scorelbl.TabIndex = 39
+        p10scorelbl.Text = "0"
+        ' 
+        ' scorepanel
+        ' 
+        scorepanel.Controls.Add(p10scorelbl)
+        scorepanel.Controls.Add(p9scorelbl)
+        scorepanel.Controls.Add(p8scorelbl)
+        scorepanel.Controls.Add(p7scorelbl)
+        scorepanel.Controls.Add(p6scorelbl)
+        scorepanel.Controls.Add(p5scorelbl)
+        scorepanel.Controls.Add(p4scorelbl)
+        scorepanel.Controls.Add(p3scorelbl)
+        scorepanel.Controls.Add(p2scorelbl)
+        scorepanel.Controls.Add(p1scorelbl)
+        scorepanel.Location = New Point(264, 345)
+        scorepanel.Name = "scorepanel"
+        scorepanel.Size = New Size(41, 242)
+        scorepanel.TabIndex = 40
+        ' 
+        ' p1timelbl
+        ' 
+        p1timelbl.AutoSize = True
+        p1timelbl.Location = New Point(13, 16)
+        p1timelbl.Name = "p1timelbl"
+        p1timelbl.Size = New Size(34, 15)
+        p1timelbl.TabIndex = 40
+        p1timelbl.Text = "00:00"
+        ' 
+        ' p2timelbl
+        ' 
+        p2timelbl.AutoSize = True
+        p2timelbl.Location = New Point(13, 36)
+        p2timelbl.Name = "p2timelbl"
+        p2timelbl.Size = New Size(34, 15)
+        p2timelbl.TabIndex = 41
+        p2timelbl.Text = "00:00"
+        ' 
+        ' p3timelbl
+        ' 
+        p3timelbl.AutoSize = True
+        p3timelbl.Location = New Point(13, 56)
+        p3timelbl.Name = "p3timelbl"
+        p3timelbl.Size = New Size(34, 15)
+        p3timelbl.TabIndex = 42
+        p3timelbl.Text = "00:00"
+        ' 
+        ' p6timelbl
+        ' 
+        p6timelbl.AutoSize = True
+        p6timelbl.Location = New Point(13, 116)
+        p6timelbl.Name = "p6timelbl"
+        p6timelbl.Size = New Size(34, 15)
+        p6timelbl.TabIndex = 45
+        p6timelbl.Text = "00:00"
+        ' 
+        ' p5timelbl
+        ' 
+        p5timelbl.AutoSize = True
+        p5timelbl.Location = New Point(13, 96)
+        p5timelbl.Name = "p5timelbl"
+        p5timelbl.Size = New Size(34, 15)
+        p5timelbl.TabIndex = 44
+        p5timelbl.Text = "00:00"
+        ' 
+        ' p4timelbl
+        ' 
+        p4timelbl.AutoSize = True
+        p4timelbl.Location = New Point(13, 76)
+        p4timelbl.Name = "p4timelbl"
+        p4timelbl.Size = New Size(34, 15)
+        p4timelbl.TabIndex = 43
+        p4timelbl.Text = "00:00"
+        ' 
+        ' p9timelbl
+        ' 
+        p9timelbl.AutoSize = True
+        p9timelbl.Location = New Point(13, 176)
+        p9timelbl.Name = "p9timelbl"
+        p9timelbl.Size = New Size(34, 15)
+        p9timelbl.TabIndex = 48
+        p9timelbl.Text = "00:00"
+        ' 
+        ' p8timelbl
+        ' 
+        p8timelbl.AutoSize = True
+        p8timelbl.Location = New Point(13, 156)
+        p8timelbl.Name = "p8timelbl"
+        p8timelbl.Size = New Size(34, 15)
+        p8timelbl.TabIndex = 47
+        p8timelbl.Text = "00:00"
+        ' 
+        ' p7timelbl
+        ' 
+        p7timelbl.AutoSize = True
+        p7timelbl.Location = New Point(13, 136)
+        p7timelbl.Name = "p7timelbl"
+        p7timelbl.Size = New Size(34, 15)
+        p7timelbl.TabIndex = 46
+        p7timelbl.Text = "00:00"
+        ' 
+        ' p10timelbl
+        ' 
+        p10timelbl.AutoSize = True
+        p10timelbl.Location = New Point(13, 196)
+        p10timelbl.Name = "p10timelbl"
+        p10timelbl.Size = New Size(34, 15)
+        p10timelbl.TabIndex = 49
+        p10timelbl.Text = "00:00"
+        ' 
+        ' timepanel
+        ' 
+        timepanel.Controls.Add(p10timelbl)
+        timepanel.Controls.Add(p9timelbl)
+        timepanel.Controls.Add(p8timelbl)
+        timepanel.Controls.Add(p7timelbl)
+        timepanel.Controls.Add(p6timelbl)
+        timepanel.Controls.Add(p5timelbl)
+        timepanel.Controls.Add(p4timelbl)
+        timepanel.Controls.Add(p3timelbl)
+        timepanel.Controls.Add(p2timelbl)
+        timepanel.Controls.Add(p1timelbl)
+        timepanel.Location = New Point(317, 344)
+        timepanel.Name = "timepanel"
+        timepanel.Size = New Size(59, 236)
+        timepanel.TabIndex = 50
         ' 
         ' HighScoresForm
         ' 
@@ -71,7 +537,10 @@ Partial Class HighScoresForm
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(84), CByte(85), CByte(84))
         ClientSize = New Size(1512, 876)
-        Controls.Add(ListBox1)
+        Controls.Add(timepanel)
+        Controls.Add(scorepanel)
+        Controls.Add(namepanel)
+        Controls.Add(rankingpanel)
         Controls.Add(backtomainbtn)
         Controls.Add(WaterBoarder)
         HelpButton = True
@@ -80,10 +549,62 @@ Partial Class HighScoresForm
         Text = "High Scores"
         TopMost = True
         CType(WaterBoarder, ComponentModel.ISupportInitialize).EndInit()
+        rankingpanel.ResumeLayout(False)
+        rankingpanel.PerformLayout()
+        namepanel.ResumeLayout(False)
+        namepanel.PerformLayout()
+        scorepanel.ResumeLayout(False)
+        scorepanel.PerformLayout()
+        timepanel.ResumeLayout(False)
+        timepanel.PerformLayout()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents WaterBoarder As PictureBox
     Friend WithEvents backtomainbtn As Button
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents p1namelbl As Label
+    Friend WithEvents p3namelbl As Label
+    Friend WithEvents p2namelbl As Label
+    Friend WithEvents ranklbl1 As Label
+    Friend WithEvents ranklbl2 As Label
+    Friend WithEvents ranklbl3 As Label
+    Friend WithEvents ranklbl4 As Label
+    Friend WithEvents ranklbl5 As Label
+    Friend WithEvents ranklbl6 As Label
+    Friend WithEvents ranklbl7 As Label
+    Friend WithEvents ranklbl8 As Label
+    Friend WithEvents ranklbl9 As Label
+    Friend WithEvents ranklbl10 As Label
+    Friend WithEvents p4namelbl As Label
+    Friend WithEvents p5namelbl As Label
+    Friend WithEvents p6namelbl As Label
+    Friend WithEvents p7namelbl As Label
+    Friend WithEvents p8namelbl As Label
+    Friend WithEvents p9namelbl As Label
+    Friend WithEvents p10namelbl As Label
+    Friend WithEvents rankingpanel As Panel
+    Friend WithEvents namepanel As Panel
+    Friend WithEvents p1scorelbl As Label
+    Friend WithEvents p2scorelbl As Label
+    Friend WithEvents p3scorelbl As Label
+    Friend WithEvents p4scorelbl As Label
+    Friend WithEvents p5scorelbl As Label
+    Friend WithEvents p6scorelbl As Label
+    Friend WithEvents p7scorelbl As Label
+    Friend WithEvents p8scorelbl As Label
+    Friend WithEvents p9scorelbl As Label
+    Friend WithEvents p10scorelbl As Label
+    Friend WithEvents scorepanel As Panel
+    Friend WithEvents p1timelbl As Label
+    Friend WithEvents p2timelbl As Label
+    Friend WithEvents p3timelbl As Label
+    Friend WithEvents p6timelbl As Label
+    Friend WithEvents p5timelbl As Label
+    Friend WithEvents p4timelbl As Label
+    Friend WithEvents p9timelbl As Label
+    Friend WithEvents p8timelbl As Label
+    Friend WithEvents p7timelbl As Label
+    Friend WithEvents p10timelbl As Label
+    Friend WithEvents timepanel As Panel
 End Class
