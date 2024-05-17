5e062947d9aada18e6ff6fe6fa6bf77190b40aec
Ben Monro <benjamin@benmonro.net>
2024-05-17 11:57:06 +1000

    XML comments for gameOverForm

--- Battleship/gameOverForm.vb
+++ Battleship/gameOverForm.vb
@@ -1,19 +1,26 @@
 ﻿Public Class gameOverForm
-
+    ''' <summary>
+    '''  subroutine called upon the form's load to call the subroutine to initialise controls
+    ''' </summary>
+    ''' <param name="sender">Reference to the control which called the subroutine</param>
+    ''' <param name="e">Provides more information about the event that caused this subroutine to be called</param>
     Private Sub gameOverForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         onLoadsettings()
     End Sub
+    ''' <summary>
+    ''' Subroutine which sets initialises the variable holding the outcome of the game and passes it into initialiseFormControls()
+    ''' </summary>
     Public Sub onLoadsettings()
         formID = "gameOverForm"
-        Dim playerscore As Integer
-        Dim playerTime As String
         Dim outcomeOfGame As String
         outcomeOfGame = gameOutcome
-        playerscore = endScore
-        playerTime = endTime
 
         initialiseFormControls(outcomeOfGame)
     End Sub
+    ''' <summary>
+    ''' Subroutine which initialises the placement, sizes, images, text etc. of the controls on the form
+    ''' </summary>
+    ''' <param name="outcomeOfGame">Who won the game. Determines what is shown on the form.</param>
     Public Sub initialiseFormControls(outcomeOfGame As String)
         Me.WindowState = FormWindowState.Maximized
         Me.Width = Screen.PrimaryScreen.Bounds.Width
@@ -49,14 +56,27 @@
 
         End If
     End Sub
-
+    ''' <summary>
+    ''' Subroutine which calls the EnterOverSmallButton() from the highscores form upon moving on the button
+    ''' </summary>
+    ''' <param name="sender">Reference to the control which called the subroutine</param>
+    ''' <param name="e">Provides more information about the event that caused this subroutine to be called</param>
     Private Sub backtomainbtn_Enter(sender As Object, e As EventArgs) Handles backtomainbtn.MouseEnter
         HighScoresForm.EnterOverSmallButton("backtomainbtn", Me)
     End Sub
+    ''' <summary>
+    ''' Subroutine which calls the ExitOverSmallButton() from the highscores form upon moving off the button
+    ''' </summary>
+    ''' <param name="sender">Reference to the control which called the subroutine</param>
+    ''' <param name="e">Provides more information about the event that caused this subroutine to be called</param>
     Private Sub backtomainbtn_Leave(sender As Object, e As EventArgs) Handles backtomainbtn.MouseLeave
         HighScoresForm.ExitOverSmallButton("backtomainbtn", Me)
     End Sub
-
+    ''' <summary>
+    ''' Subroutine which takes the user to the main menu form.
+    ''' </summary>
+    ''' <param name="sender">Reference to the control which called the subroutine</param>
+    ''' <param name="e">Provides more information about the event that caused this subroutine to be called</param>
     Private Sub backtomainbtn_Click(sender As Object, e As EventArgs) Handles backtomainbtn.Click
         'When Exit button is clicked
         Me.Hide()
